c587b3f27f5d8f645fa576615c22ba056145ea80
Daniel Macedo <danielmsv2011@hotmail.com>
2022-05-20 08:06:43 -0300

    Separação de templates

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
-    include_once("helpers/url.php");
+    include_once('templates/header.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog Codar</title>
-    <!-- Estilos do Projeto -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&family=Roboto&display=swap" rel="stylesheet">
-</head>
-<body>
     <h1>Olá mundo!</h1>
-</body>
-</html>
+<?php
+    include_once('templates/footer.php');
+?>
---
+++ templates/header.php
@@ -0,0 +1,31 @@
+<?php
+    include_once("helpers/url.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <!-- Estilos do Projeto -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <a href="<?= $BASE_URL ?>" id="logo"> <!-- Sempre que eu clicar no logo eu vou pra home do site -->
+            <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Blog Codar">
+        </a>
+        <nav>
+            <ul id="navbar">
+                <li><a href="<?= $BASE_URL ?>">Home</a></li>
+                <li><a href="<?= $BASE_URL ?>">Home</a></li>
+                <li><a href="<?= $BASE_URL ?>">Home</a></li>
+                <li><a href="<?= $BASE_URL ?>/contato.php">Contatos</a></li>
+            </ul>
+        </nav>
+    </header>
